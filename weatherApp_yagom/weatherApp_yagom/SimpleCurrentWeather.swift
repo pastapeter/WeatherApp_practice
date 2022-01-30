bb25d00524a59ee3595de10142843decb6867089
pastapeter <pastapeter@naver.com>
2022-01-30 23:19:02 +0900

    feat: struct SimpleCurrentWeather 추가

---
+++ weatherApp_yagom/weatherApp_yagom/SimpleCurrentWeather.swift
@@ -0,0 +1,14 @@
+//
+//  City.swift
+//  weatherApp_yagom
+//
+//  Created by abc on 2022/01/30.
+//
+
+import Foundation
+
+struct SimpleCurrentWeather {
+  var name: String
+  var currentTemperature: Double
+  var currentHumid: Double
+}
