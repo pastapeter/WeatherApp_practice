e8ce757f0fd6a2b59344eefe7a418114c435f45f
pastapeter <pastapeter@naver.com>
2022-01-31 18:40:42 +0900

    feat: NetworkError, NetworkRequest 추가

---
+++ weatherApp_yagom/weatherApp_yagom/Utility/NetworkUtils/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  weatherApp_yagom
+//
+//  Created by abc on 2022/01/31.
+//
+
+import Foundation
+
+enum NetworkError: String, Error {
+  case failResponse
+  case invaildData
+}
