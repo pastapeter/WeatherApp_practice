f2e47e9ef7110a22f3fdd2eaaf9c96f758c4eee8
pastapeter <pastapeter@naver.com>
2022-01-31 18:45:28 +0900

    feat: 뷰모델에 repository 주입, network로 날씨 리스트 불러오기

--- weatherApp_yagom/weatherApp_yagom/WeatherMainViewModel.swift
+++ weatherApp_yagom/weatherApp_yagom/WeatherMainViewModel.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
-final class WeatherViewModel {
+final class WeatherMainViewModel {
   
+  private let respository: CurrentWeatherRepository
+  private var dataList: [CurrentWeather] = []
+  
+  init(currentWeatherRepository: CurrentWeatherRepository) {
+    self.respository = currentWeatherRepository
+    let cityList = CityName.allCases.map {
+      $0.rawValue
+    }
+    cityList.forEach {
+      respository.currentWeather(in: $0) { [weak self] weatherInfo in
+        self?.dataList.append(weatherInfo)
+      }
+    }
+  }
+  
+
 }
