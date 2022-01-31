092a929f0ca6bf9f1d80781d947d6db5239369dc
pastapeter <pastapeter@naver.com>
2022-01-31 18:41:24 +0900

    feat: Decoding 할 수 있는 protocol, class 추가 (NetworkCoding, NetworkCoder)

---
+++ weatherApp_yagom/weatherApp_yagom/Utility/NetworkUtils/NetworkCoding.swift
@@ -0,0 +1,12 @@
+//
+//  UserSessionCoding.swift
+//  weatherApp_yagom
+//
+//  Created by abc on 2022/01/31.
+//
+
+import Foundation
+
+protocol NetworkCoding {
+  func getResponse<T: Codable>(data: Data, style: T.Type, completion: @escaping(T) -> Void)
+}
