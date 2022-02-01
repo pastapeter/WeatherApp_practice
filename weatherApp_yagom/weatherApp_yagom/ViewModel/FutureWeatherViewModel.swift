f37792209e12cd313e5f894f4a6a2660e0d7b2f9
pastapeter <pastapeter@naver.com>
2022-02-02 02:05:07 +0900

    feat: FutureWeatherViewModel에서 forecast Fetch 기능 추가

--- weatherApp_yagom/weatherApp_yagom/ViewModel/FutureWeatherViewModel.swift
+++ weatherApp_yagom/weatherApp_yagom/ViewModel/FutureWeatherViewModel.swift
@@ -11,10 +11,29 @@ final class FutureWeatherViewModel {
   
   private let futureWeatherRepository: FutureWeatherRepository
   let cityname: String
+  var updateUI: (() -> Void)?
   
   public init(futureWeatherRepository: FutureWeatherRepository, cityName: String) {
     self.futureWeatherRepository = futureWeatherRepository
     self.cityname = cityName
+    getForecast()
+  }
+  
+  var forecast: [(time: String, tempMax: Double, tempMin: Double, humid: Double)] = [] {
+    didSet {
+      updateUI?()
+    }
+  }
+  
+  func getForecast() {
+    self.futureWeatherRepository.futureWeather(in: self.cityname) { [weak self] weather in
+      guard let self = self else {return}
+      var temp :  [(time: String, tempMax: Double, tempMin: Double, humid: Double)] = []
+      weather.list.forEach {
+        temp.append((time: $0.date.forMattingDate(), tempMax: $0.main.tempMax, tempMin: $0.main.tempMin, humid: $0.main.humidity ))
+      }
+      self.forecast = temp
+    }
   }
   
 }
