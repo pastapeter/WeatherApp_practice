b869317da0dc91a2f5f551093069f9be0a709faf
pastapeter <pastapeter@naver.com>
2022-02-02 14:18:29 +0900

    feat: WeatherDetailDependencyContainer에 ImageCache 넘겨줌

--- weatherApp_yagom/weatherApp_yagom/DependencyContainer/WeatherDetailDependencyContainer.swift
+++ weatherApp_yagom/weatherApp_yagom/DependencyContainer/WeatherDetailDependencyContainer.swift
@@ -11,6 +11,7 @@ final class WeatherDetailDependencyContainer {
   
   // 부모 container로부터 받는 의존성
   let sharedWeatherRepository: CurrentWeatherRepository
+  let imageCacher: ImageCache
   
   // 상태저장 의존성
   let sharedWeatherDetailViewModel: WeatherDetailViewModel
@@ -22,12 +23,12 @@ final class WeatherDetailDependencyContainer {
     }
     
     self.sharedWeatherRepository = appDependencyContainer.sharedWeatherRepository
+    self.imageCacher = appDependencyContainer.imageCache
     self.sharedWeatherDetailViewModel = makeWeatherDetailViewModel(weatherRepository: sharedWeatherRepository, cityName: cityName)
-    
   }
   
   func makeWeatherDetailViewController() -> WeatherDetailViewController {
-    return WeatherDetailViewController(viewModel: sharedWeatherDetailViewModel, futureWeatherViewControllerFactory: self)
+    return WeatherDetailViewController(viewModel: sharedWeatherDetailViewModel, futureWeatherViewControllerFactory: self, imageCacher: imageCacher)
   }
   
   // FutureWeather
