080017af872bb5a5d1f427c770f8756785dc0c6a
pastapeter <pastapeter@naver.com>
2022-02-27 15:52:34 +0900

    refactor: WeatherDetailVC를 rxswift로 refactor

--- weatherApp_yagom/weatherApp_yagom/View/WeatherDetail/WeatherDetailViewController.swift
+++ weatherApp_yagom/weatherApp_yagom/View/WeatherDetail/WeatherDetailViewController.swift
@@ -6,20 +6,46 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class WeatherDetailViewController: UIViewController {
+class WeatherDetailViewController: UIViewController, ViewModelBindableType {
   
   //MARK: - UI
   
-  private var datasource: [String] = ["도시이름", "", "현재온도: ", "체감온도: ", "현재습도: ", "기압: ",
-                                      "최고기온: ", "최저기온: ", "풍속: ", "오늘의 날씨는"]
+  private var titlePrefix: [String] = ["", "", "현재온도: ", "체감온도: ", "현재습도: ", "기압: ",
+                                      "최고기온: ", "최저기온: ", "풍속: ", "오늘의 날씨는 "]
   
   private var tableView = UITableView()
   lazy var futureWeatherButton: UIBarButtonItem = {
     let button = UIBarButtonItem(title: "미래날씨", style: .plain, target: self, action: #selector(gotoFutureWeatherVC(_:)))
     return button
   }()
+
+  //MARK: - Binding
   
+  func bindViewModel() {
+    viewModel.weatherInfo
+      .drive(tableView.rx.items(cellIdentifier: WeatherDetailTableViewCell.identifier, cellType: WeatherDetailTableViewCell.self)) { [weak self] (index, element, cell) in
+        guard let self = self else {return}
+        if index == 1 {
+          self.imageCacher.getIcon(with: self.titlePrefix[index] + element) { (image) in
+            if let image = image {
+              cell.iconImageView.image = image
+            }
+          }
+          cell.stackView.alignment = .center
+          cell.iconImageView.isHidden = false
+          cell.titleLabel.text = nil
+        } else {
+          cell.titleLabel.text = self.titlePrefix[index] + element
+          cell.iconImageView.isHidden = true
+          cell.stackView.alignment = .leading
+        }
+      }
+      .disposed(by: disposeBag)
+  }
+
   //MARK: - init
 
   public init(viewModel: WeatherDetailViewModel, futureWeatherViewControllerFactory: FutureWeatherViewControllerFactory, imageCacher: ImageCache) {
@@ -27,23 +53,7 @@ class WeatherDetailViewController: UIViewController {
     self.viewModel = viewModel
     self.futureWeatherViewControllerFactory = futureWeatherViewControllerFactory
     super.init(nibName: nil, bundle: nil)
-    
-    self.viewModel.updateUI = { [weak self] in
-      guard let self = self else {return}
-      DispatchQueue.main.async {
-        self.datasource[0] = self.viewModel.cityName
-        self.datasource[1] = self.viewModel.weatherInfo.imageUrl
-        self.datasource[2] = "현재온도: \(self.viewModel.weatherInfo.temp)℃"
-        self.datasource[3] = "체감온도: \(self.viewModel.weatherInfo.feelsLike)℃"
-        self.datasource[4] = "현재습도: \(self.viewModel.weatherInfo.humidity)%"
-        self.datasource[5] = "기압: \(self.viewModel.weatherInfo.pressure)hPa"
-        self.datasource[6] = "최고온도: \(self.viewModel.weatherInfo.tempMax)℃"
-        self.datasource[7] = "최저기온: \(self.viewModel.weatherInfo.tempMin)℃"
-        self.datasource[8] = "풍속: \(self.viewModel.weatherInfo.windSpeed)m/s"
-        self.datasource[9] = "오늘의 날씨는 \(self.viewModel.weatherInfo.description)"
-        self.tableView.reloadData()
-      }
-    }
+    bind(viewModel: self.viewModel)
   }
   
   required init?(coder: NSCoder) {
@@ -57,7 +67,6 @@ class WeatherDetailViewController: UIViewController {
     view.backgroundColor = .white
     setupBarbutton()
     setupTableView()
-    // Do any additional setup after loading the view.
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -67,9 +76,10 @@ class WeatherDetailViewController: UIViewController {
   
   //MARK: - Private
   
-  private var viewModel: WeatherDetailViewModel
+  var viewModel: WeatherDetailViewModel
   private var futureWeatherViewControllerFactory: FutureWeatherViewControllerFactory
-  private weak var imageCacher: ImageCache?
+  private var imageCacher: ImageCache
+  private var disposeBag = DisposeBag()
   
   private func setupBarbutton() {
     navigationItem.rightBarButtonItem = futureWeatherButton
@@ -82,9 +92,6 @@ class WeatherDetailViewController: UIViewController {
     tableView.register(WeatherDetailTableViewCell.self)
     tableView.translatesAutoresizingMaskIntoConstraints = false
     view.addSubview(tableView)
-    
-    tableView.delegate = self
-    tableView.dataSource = self
     
     tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
     tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
@@ -100,40 +107,3 @@ class WeatherDetailViewController: UIViewController {
   
 }
 
-//MARK: - Datasource
-
-extension WeatherDetailViewController: UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return datasource.count
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    
-    guard let cell = tableView.dequeueReusableCell(withIdentifier: WeatherDetailTableViewCell.identifier, for: indexPath) as? WeatherDetailTableViewCell else { return WeatherDetailTableViewCell()}
-    
-    if indexPath.row == 1 { //icon
-      if datasource[indexPath.row] != "" {
-        imageCacher?.getIcon(with: datasource[indexPath.row]) { (image) in
-          if let image = image {
-            cell.iconImageView.image = image
-          }
-        }
-      }
-      cell.stackView.alignment = .center
-      cell.iconImageView.isHidden = false
-      cell.titleLabel.text = nil
-    } else {
-      cell.iconImageView.isHidden = true
-      cell.titleLabel.text = datasource[indexPath.row]
-      cell.stackView.alignment = .leading
-    }
-    
-    return cell
-  }
-  
-}
-
-//MARK: - Delegate
-
-extension WeatherDetailViewController: UITableViewDelegate {
-}
